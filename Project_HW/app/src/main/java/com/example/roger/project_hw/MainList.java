9ed7c1be9d84f150fbcfed1fe3e744543368572d
Roger Wang <roger35972134@gmail.com>
2015-12-02 23:25:25 +0800

    12/2 update-2
    Camera cancel bugs fix

--- Project_HW/app/src/main/java/com/example/roger/project_hw/MainList.java
+++ Project_HW/app/src/main/java/com/example/roger/project_hw/MainList.java
@@ -24,29 +24,21 @@ import butterknife.OnClick;
 
 public class MainList extends Activity {
     String filepath=Environment.getExternalStorageDirectory().getAbsolutePath()+"/Pictures/HW/Photo";
-    MyAdapter adapter;
     MyRecycler myRecycler;
     ArrayList<String> Myfiles=new ArrayList<>();
     ArrayList<String> Files=new ArrayList<>();
+    File f;
+    boolean flag=false;
 
     //@Bind(R.id.list) ListView listView;
     @Bind(R.id.id_recyclerview_horizontal) RecyclerView recyclerView;
 
     @OnClick(R.id.captureimage) void take_photo(){
-        File f=dispatchTakePictureIntent();
-        Myfiles.add(f.getName());
+        f=dispatchTakePictureIntent();
+       /* Myfiles.add(f.getName());
         Files.add(f.getAbsolutePath());
-        myRecycler.notifyDataSetChanged();
+        myRecycler.notifyDataSetChanged();*/
     }
-    /*@OnItemClick(R.id.list) void itemClick(AdapterView<?> parent, View v, int position, long id){
-        Intent intent = new Intent();
-        intent.setClass(MainList.this, MyPhoto.class);
-        Bundle bundle = new Bundle();
-        bundle.putString("PATH", Files.get(position));
-        bundle.putString("NAME", Myfiles.get(position));
-        intent.putExtras(bundle);
-        startActivity(intent);
-    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,8 +73,6 @@ public class MainList extends Activity {
             }
         });
         recyclerView.setAdapter(myRecycler);
-        //adapter=new MyAdapter(this,Myfiles,Files,this);
-        //listView.setAdapter(adapter);
     }
 
     String mCurrentPhotoPath;
@@ -92,8 +82,6 @@ public class MainList extends Activity {
         String imageFileName = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File storageDir = Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES + "/HW/Photo/");
-        /*if(!storageDir.exists())
-            storageDir.mkdirs();*/
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -114,7 +102,6 @@ public class MainList extends Activity {
             File photoFile = null;
             try {
                 photoFile = createImageFile();
-
             } catch (IOException ex) {
                 // Error occurred while creating the File
                 Toast toast=Toast.makeText(this,"Error!",Toast.LENGTH_LONG);
@@ -130,7 +117,15 @@ public class MainList extends Activity {
         }
         return null;
     }
-
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(resultCode==RESULT_OK)
+        {
+            Myfiles.add(f.getName());
+            Files.add(f.getAbsolutePath());
+            myRecycler.notifyDataSetChanged();
+        }
+        super.onActivityResult(requestCode,resultCode,data);
+    }
 }
 
 
